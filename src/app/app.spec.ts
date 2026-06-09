6b1e38cedebbe5b7b6ac9bf01e20c2bfce67d764
Mykhailo B <mykhailo.bilousov.shi.2024@lpnu.ua>
2026-06-09 12:47:54 +0300

    test: add unit test for app component

---
+++ src/app/app.spec.ts
@@ -0,0 +1,10 @@
+import '@angular/compiler'
+import { describe, it, expect } from 'vitest'
+import { App } from './app'
+
+describe('App', () => {
+  it('has the expected title', () => {
+    const app = new App()
+    expect(app.title()).toBe('Client')
+  })
+})
