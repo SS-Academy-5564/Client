ecee03c553bd251767550442e88382b984ada1a3
Sasha Shyshkanova <ivanovasasha883@gmail.com>
2026-06-15 14:26:08 +0300

    Add Register page

---
+++ src/app/core/models/register-model.ts
@@ -0,0 +1,12 @@
+export interface RegisterRequest {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export interface AuthResponse {
+    isLoading: boolean;
+    error: string | null;
+}
---
+++ src/app/shared/ui/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, RouterModule],
+  templateUrl: './button.html',
+  styleUrls: ['./button.scss'],
+})
+export class ButtonComponent {
+  @Input() label = '';
+  @Input() type: 'primary' | 'secondary' | 'text' = 'primary';
+  @Input() variant: 'default' | 'nav' | 'full-width' = 'default';
+  @Input() disabled = false;
+  @Input() routerLink?: string;
+}
