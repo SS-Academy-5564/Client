1211e9ee2d2869775f749342cff0702eb57bfce5
Artem Koval <kovalartem9010@gmail.com>
2026-06-22 17:29:50 +0300

    chore: update eslint configuration for HTML files

--- eslint.config.js
+++ eslint.config.js
@@ -35,7 +35,6 @@ module.exports = defineConfig([
   },
   {
     files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility]
   },
 ]);
