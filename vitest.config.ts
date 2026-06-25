6b05efc398e0cb5a707215bf847672a5b5b59849
Sasha Shyshkanova <ivanovasasha883@gmail.com>
2026-06-25 12:56:51 +0300

    fix: resolve TS paths and Vitest config issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
 /// <reference types="vitest" />
+import { resolve } from 'path/win32';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@core': resolve(__dirname, 'src/app/core'),
+      '@shared': resolve(__dirname, 'src/app/shared'),
+      '@features': resolve(__dirname, 'src/app/features'),
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
